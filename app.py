eec34047cc95a86acbbb34f2002b9fd49dda4e3a
defve1988 <zhouchuanlong1988@gmail.com>
2020-05-21 23:24:31 +0200

    normal change

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def return_home():
     return "This is Defve-app's backend."
 
-
-app.run()
+if __name__=='__main__':
+    app.run()
