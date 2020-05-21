e10b3a093bc02be6bbf83c2f316c094a68d12369
defve1988 <zhouchuanlong1988@gmail.com>
2020-05-21 23:35:52 +0200

    normal change

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, request, jsonify
 
+from do import *
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def return_home():
     return "This is Defve-app's backend."
+
+@app.route('/get',methods=['GET'])
+def get_test():
+    u ='https://www.youtube.com/watch?v=gyBkHYWNsv4'
+    title, url = video_title(u)
+    res = {"title":title,
+            "url":url}
+    return jsonify(res) 
 
 if __name__=='__main__':
     app.run()
---
+++ do.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+
+def video_title(url):
+    source= requests.get(url).text
+    soup=BeautifulSoup(source,'lxml')
+    div_s = soup.findAll('div')
+    Title = div_s[1].find('span',class_='watch-title').text.strip()
+    return Title,url
